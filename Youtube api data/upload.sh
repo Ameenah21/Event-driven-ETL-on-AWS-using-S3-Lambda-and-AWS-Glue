70720b693d87f8ae5105666601f840a570b5c61b
Ameenah21 <aminatlawal21@gmail.com>
2023-06-09 16:19:55 +0100

    Project files

---
+++ Youtube api data/upload.sh
@@ -0,0 +1,10 @@
+aws s3 cp CAvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://youtube-de-project12/youtube/raw_statistics/region=us/
